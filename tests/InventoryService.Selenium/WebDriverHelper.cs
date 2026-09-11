e53b1c7b7dd871172124aa6ae9f318d5cc8a3404
dilumpeiris <65111391+dilumpeiris@users.noreply.github.com>
2026-09-11 12:22:13 +0530

    tests(selenium): add InventoryService.Selenium project and webdriver helper

---
+++ tests/InventoryService.Selenium/WebDriverHelper.cs
@@ -0,0 +1,31 @@
+using OpenQA.Selenium.Chrome;
+using Xunit;
+
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
+
+namespace InventoryService.SeleniumTests
+{
+    public static class WebDriverHelper
+    {
+        public static ChromeOptions CreateOptions()
+        {
+            var options = new ChromeOptions();
+
+            bool isCi = string.Equals(Environment.GetEnvironmentVariable("CI"), "true", StringComparison.OrdinalIgnoreCase);
+            if (isCi)
+            {
+                options.AddArgument("--headless=new");
+                options.AddArgument("--no-sandbox");
+                options.AddArgument("--disable-dev-shm-usage");
+                options.AddArgument("--disable-gpu");
+                options.AddArgument("--window-size=1920,1080");
+            }
+            else
+            {
+                options.AddArgument("--start-maximized");
+            }
+
+            return options;
+        }
+    }
+}
