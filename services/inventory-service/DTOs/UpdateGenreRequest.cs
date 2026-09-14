e375073ab862d86ab886facf48e00e548d8ae1d4
iamthisal <thisaldilnath@gmail.com>
2026-09-14 13:26:41 +0530

    feat(inventory): add genre DTOs

---
+++ services/inventory-service/DTOs/UpdateGenreRequest.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace InventoryService.DTOs
+{
+    public class UpdateGenreRequest
+    {
+        [Required(ErrorMessage = "Genre name is required")]
+        [StringLength(100, MinimumLength = 1, ErrorMessage = "Genre name must be between 1 and 100 characters")]
+        public string Name { get; set; } = string.Empty;
+    }
+}
