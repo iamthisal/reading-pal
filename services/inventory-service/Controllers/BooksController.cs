279e0ba2709beb63b9475a467ed4af36e49783f3
iamthisal <thisaldilnath@gmail.com>
2026-09-14 10:37:08 +0530

    fix(inventory): select Kafka-enabled controller constructor

--- services/inventory-service/Controllers/BooksController.cs
+++ services/inventory-service/Controllers/BooksController.cs
@@ -22,6 +22,7 @@ namespace InventoryService.Controllers
         {
         }
 
+        [ActivatorUtilitiesConstructor]
         public BooksController(
             InventoryDbContext context,
             IBookEventPublisher bookEventPublisher,
