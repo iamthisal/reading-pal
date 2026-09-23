63afa7a5478cfab6f50b27dc6f54b81b79664259
DahamKu <it24101516@my.sliit.lk>
2026-09-23 09:37:39 +0530

    YRP-201: feat(lending): add borrow records and database migration

---
+++ services/lending-service/Models/BorrowRecord.cs
@@ -0,0 +1,12 @@
+namespace LendingService.Models;
+
+public class BorrowRecord
+{
+    public int Id { get; set; }
+    public int ReservationId { get; set; }
+    public int BookId { get; set; }
+    public int UserId { get; set; }
+    public DateTime CheckoutDate { get; set; }
+    public DateTime DueDate { get; set; }
+    public DateTime? ReturnDate { get; set; }
+}
